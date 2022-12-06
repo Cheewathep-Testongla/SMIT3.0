1f910065349aa6355551d5e6808be74710764ce1
s6204062616049 <nongton2544@gmail.com>
2022-12-06 13:40:26 +0700

    Fixing SQL Connection

--- app/CleansingAuditData/getRiskScore.py
+++ app/CleansingAuditData/getRiskScore.py
@@ -5,7 +5,14 @@ import pandas as pd
 
 def getRiskCount(Type):
     if Type != '-':
-        RiskCount = pd.read_sql("SELECT COUNT([TypeOfFinding])FROM [dbo].[Cleansing_FindingDetails] WHERE TypeOfFinding = '"+Type+"' GROUP BY TypeOfFinding ;", 
-                                connection_SMIT3)
-        RiskCount = RiskCount[''].tolist()[0]
-        return RiskCount
+        try:
+            print("Used Data source from SQL Server")
+            RiskCount = pd.read_sql("SELECT COUNT([TypeOfFinding])FROM [dbo].[Cleansing_FindingDetails] WHERE TypeOfFinding = '"+Type+"' GROUP BY TypeOfFinding ;", 
+                                    connection_SMIT3)
+            RiskCount = RiskCount[''].tolist()[0]
+            return RiskCount
+        except:
+            print("Used Data source from CSV")
+            RiskCount = pd.read_csv('./SMIT_Data/Prepared_Safety_Audit.csv', encoding='utf-8')
+            RiskCount = (RiskCount.loc[RiskCount["TypeOfFinding"] == type, "TypeOfFinding"]).tolist() 
+            return len(RiskCount)
